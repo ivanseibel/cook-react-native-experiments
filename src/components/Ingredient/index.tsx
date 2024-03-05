963a30141f715b1158d53a9d3b1bdf2cc30502b3
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-05 19:32:09 +0000

    Refactor Ingredient component to accept additional props

--- src/components/Ingredient/index.tsx
+++ src/components/Ingredient/index.tsx
@@ -1,14 +1,29 @@
-import { Image, Pressable, Text } from "react-native";
+import { Image, Pressable, PressableProps, Text } from "react-native";
 import styles from "./styles";
 
-export const Ingredient = () => {
+type ComponentProps = PressableProps & {
+	name: string;
+	image: string;
+	onPress?: () => void;
+	selected?: boolean;
+};
+
+export const Ingredient = ({
+	name,
+	image,
+	selected = false,
+	...rest
+}: ComponentProps) => {
 	return (
-		<Pressable style={styles.container}>
+		<Pressable
+			style={[styles.container, selected && styles.selected]}
+			{...rest}
+		>
 			<Image
 				style={styles.image}
 				source={require("@/assets/images/apple.png")}
 			/>
-			<Text style={styles.text}>Apple</Text>
+			<Text style={styles.text}>{name}</Text>
 		</Pressable>
 	);
 };
