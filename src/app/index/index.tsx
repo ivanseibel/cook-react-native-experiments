5873e3f05aff8bb35f0ab60b748ccf7496b73aeb
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-05 18:30:41 +0000

    Add Ingredient component and update title and subtitle

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
 import styles from "./styles";
+import { Ingredient } from "@/components/Ingredient";
 
 const Index = () => {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Hello, World!</Text>
+			<Text style={styles.title}>
+				Select {"\n"}
+				<Text style={styles.subtitle}>the ingredients</Text>
+			</Text>
+			<Text style={styles.subtitle}>
+				Unleash culinary adventures with recipes tailored to your chosen
+				ingredients!
+			</Text>
+
+			<Ingredient />
 		</View>
 	);
 };
