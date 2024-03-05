59ddc7ecae3d1666a2da5fc45957528fa0b4a00c
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-05 16:05:38 +0000

    Delete unused files and add new index screen

---
+++ src/app/index/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+const Index = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Hello, World!</Text>
+		</View>
+	);
+};
+
+export default Index;
