924fa84a1a89333e848a65182399fb132c5875af
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-06 18:19:56 +0000

    Refactor styles in recipes component

--- src/app/recipes/styles.ts
+++ src/app/recipes/styles.ts
@@ -4,23 +4,23 @@ import { theme } from "@/theme";
 export default StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingHorizontal: 32,
+		alignContent: "flex-start",
 	},
 	header: {
+		paddingHorizontal: 32,
 		paddingTop: 62,
-		marginBottom: 22,
+		marginBottom: 12,
 	},
 	title: {
 		fontSize: theme.fonts.size.body.md,
 		fontFamily: theme.fonts.family.bold,
 		lineHeight: 18,
-	},
-	ingredientList: {
-		marginTop: 12,
-		gap: 6,
+		marginTop: 22,
+		paddingHorizontal: 32,
 	},
 	recipes: {
-		padding: 32,
+		paddingHorizontal: 32,
+		paddingVertical: 24,
 	},
 	recipeList: {
 		gap: 12,
