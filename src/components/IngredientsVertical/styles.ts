52e4d70acd45e304677d957a79e44de2f4a622ec
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-06 18:20:19 +0000

    Add styles for IngredientsVertical component

---
+++ src/components/IngredientsVertical/styles.ts
@@ -0,0 +1,12 @@
+import { theme } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+	container: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		paddingBottom: 200,
+		gap: 6,
+		marginTop: 24,
+	},
+});
