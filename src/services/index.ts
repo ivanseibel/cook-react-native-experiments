555e064723714278819fc5af8ce86315a46eff25
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-06 18:20:24 +0000

    Add new services for recipes and preparations

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import * as ingredients from "./ingredients.service";
+import * as recipes from "./recipes.service";
+import * as preparations from "./preparations.service";
 
 export const services = {
 	ingredients,
+	recipes,
+	preparations,
 	storage: {
 		path: "https://zhtzqcfmiaszsjszqdny.supabase.co/storage/v1/object/public/ingredients/",
 	},
