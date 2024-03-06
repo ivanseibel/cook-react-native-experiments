19a5a860801505e61d02b2705af192166e431be5
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-06 18:20:12 +0000

    Add styles for IngredientsHorizontal component

---
+++ src/components/IngredientsHorizontal/styles.ts
@@ -0,0 +1,12 @@
+import { theme } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+	ingredientListContainer: {
+		height: 45,
+		maxHeight: 45,
+	},
+	ingredientListContent: {
+		gap: 6,
+	},
+});
