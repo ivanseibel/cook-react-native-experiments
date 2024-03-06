30540f140fcbb759259360170fa32194a8fc0a55
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-06 19:19:29 +0000

    Add steps and preparation to recipe view

--- src/app/recipe/[id].tsx
+++ src/app/recipe/[id].tsx
@@ -1,75 +1,85 @@
-import { Image, StatusBar, Text, View } from "react-native";
+import { FlatList, Image, StatusBar, Text, View } from "react-native";
 import styles from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { IngredientsHorizontal } from "@/components/IngredientsHorizontal";
+import { Step } from "@/components/Step";
+import { useEffect, useState } from "react";
+import { findByRecipeId } from "../../services/ingredients.service";
+import { services } from "@/services";
 
-const ingredients = [
+const steps = [
 	{
-		id: "1",
-		name: "Pasta",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 1,
+		description: "Boil water in a large pot.",
 	},
 	{
-		id: "2",
-		name: "Tomato",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 2,
+		description: "Add the pasta and cook for 10 minutes.",
 	},
 	{
-		id: "3",
-		name: "Garlic",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 3,
+		description: "In a pan, heat the olive oil and add the garlic.",
 	},
 	{
-		id: "4",
-		name: "Olive Oil",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 4,
+		description: "Add the tomatoes and cook for 5 minutes.",
 	},
 	{
-		id: "5",
-		name: "Salt",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 5,
+		description: "Season with salt and pepper.",
 	},
 	{
-		id: "6",
-		name: "Pepper",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 6,
+		description: "Add the basil and cook for 2 minutes.",
 	},
 	{
-		id: "7",
-		name: "Basil",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 7,
+		description: "Drain the pasta and add it to the pan.",
 	},
 	{
-		id: "8",
-		name: "Parmesan",
-		image:
-			"https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
+		step: 8,
+		description: "Mix well and serve with parmesan.",
 	},
 ];
 
 const Recipe = () => {
+	const [ingredients, setIngredients] = useState<IngredientResponse[]>([]);
+	const [recipe, setRecipe] = useState<RecipeResponse | null>(null);
+	const [preparation, setPreparation] = useState<PreparationsResponse[]>([]);
+
 	const router = useRouter();
+
+	const { id: recipeId } = useLocalSearchParams<{ id: string }>();
+
+	useEffect(() => {
+		services.ingredients.findByRecipeId(recipeId).then((data) => {
+			console.log("recipeId", recipeId);
+			setIngredients(data);
+			console.log("data", data);
+		});
+	}, [recipeId]);
+
+	useEffect(() => {
+		services.recipes.show(recipeId).then((data) => {
+			setRecipe(data);
+		});
+	}, [recipeId]);
+
+	useEffect(() => {
+		services.preparations.findByRecipeId(recipeId).then((data) => {
+			setPreparation(data);
+		});
+	}, [recipeId]);
 
 	return (
 		<View style={styles.mainContainer}>
 			<StatusBar barStyle="dark-content" />
-			<Image
-				style={styles.image}
-				source={{
-					uri: "https://cooknenjoy.com/wp-content/uploads/2021/11/P1340162-1800x1352.jpg",
-				}}
-			/>
+			<Image style={styles.image} source={{ uri: recipe?.image }} />
 
 			{/* Recipe Container */}
 			<View style={styles.recipeContainer}>
+				{/* Header */}
 				<View style={styles.recipeHeader}>
 					<MaterialIcons
 						name="arrow-back"
@@ -78,15 +88,33 @@ const Recipe = () => {
 					/>
 				</View>
 
-				<Text style={styles.recipeTitle}>Pasta with Tomato Sauce</Text>
+				<Text style={styles.recipeTitle}>
+					{recipe?.name ?? "Recipe not found"}
+				</Text>
 				<Text style={styles.recipePreparationTime}>
-					20 minutes preparation time
+					{recipe?.minutes ?? 0} minutes preparation time
 				</Text>
 
 				<Text style={styles.ingredientsTitle}>Ingredients</Text>
 				<IngredientsHorizontal ingredients={ingredients} />
 
 				<Text style={styles.recipePreparationTitle}>Preparation</Text>
+
+				<FlatList
+					data={preparation}
+					keyExtractor={(item) => item.id}
+					showsVerticalScrollIndicator={false}
+					style={styles.recipePreparationStepsContainer}
+					contentContainerStyle={{
+						paddingHorizontal: 12,
+						gap: 16,
+						paddingTop: 16,
+						paddingBottom: 36,
+					}}
+					renderItem={({ item }) => (
+						<Step description={item.description} step={item.step} />
+					)}
+				/>
 			</View>
 		</View>
 	);
